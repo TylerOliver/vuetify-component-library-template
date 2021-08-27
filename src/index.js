9e960d93f93da3d33378a00c537a1d04cd13d966
Tyler Oliver <toliver@campusedu.com>
2021-08-27 11:23:36 -0400

    Removed unused functions

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ const ComponentLibrary = {
       Vue.component(name, components[name]);
     });
 
-    // Add some commonly used sounds throughout the company's products
-    // and make them available via 'this.$sounds'
-    // eslint-disable-next-line no-param-reassign
-    Vue.prototype.$sounds = audio;
-
     if (options) {
       // This is needed and supported by the Vue object.
       // If we used typescript it would allow this.
